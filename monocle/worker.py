4136edcab2e54a0bb1147d277193183b6c8e3d6b
RobTwoThree <Rob@robs-mbp-3.attlocal.net>
2017-04-20 18:04:11 -0700

    Revert "Merge branch 'develop_unown' into public_front_end"
    This reverts commit d16757168a45b0a2db701547de803c9844c8fe3a, reversing
    changes made to 10b27fe8165b8001783104a46b82129f20101da6.

--- monocle/worker.py
+++ monocle/worker.py
@@ -947,14 +947,6 @@ class Worker:
             pokemon['height'] = pdata['height_m']
             pokemon['weight'] = pdata['weight_kg']
             pokemon['gender'] = pdata['pokemon_display']['gender']
-<<<<<<< HEAD
-=======
-            #Check form just for Unown
-            if pdata['pokemon_id'] == 201 and pdata['pokemon_display']['form'] > 0: 
-                pokemon['form'] = pdata['pokemon_display']['form']
-            else:
-                pokemon['form'] = None
->>>>>>> develop_unown
         except KeyError:
             self.log.error('Missing Pokemon data in encounter response.')
         self.error_code = '!'
